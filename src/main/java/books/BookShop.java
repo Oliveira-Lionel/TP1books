998511c035d1657287820859f7c8b01e9be633fd
Oliveira Lionel <lio_tuga_07@hotmail.fr>
2021-03-18 18:54:07 +0100

    initial commit

---
+++ src/main/java/books/BookShop.java
@@ -0,0 +1,54 @@
+package books;
+
+public class BookShop {
+
+    private final String name;
+
+    /**
+     * Constructor of the class Book shop
+     * @param name name of the book shop
+     */
+    public BookShop(String name){
+        this.name = name;
+    }
+
+    /**
+     * method to compute the cost of a basket
+     * @param books array corresponding to the number of each harry potter book the client desire to buy (books.length should return 5)
+     * @return the cost in euro with the discount
+     */
+    public double cost(int[] books){
+
+	int sum = 8*books.length;  //cost without discount
+        int differentBooks = 0;  //amount of books with different name
+	int sumToDiscount = 0;  //money that needs to be discount
+	
+	for(int i=0 ; i<books.length-1 ; i++) {
+		for(int j=i+1 ; j<books.length ; j++) {
+			if(!(books.get(i).equals(books.get(j)))) {
+				differentBooks++;
+			}
+		}
+	}
+	
+	if(differentBooks==2) {
+		sumToDiscount=(differentBooks*8*0.07);
+	}
+
+	if(differentBooks==3) {
+		sumToDiscount=(differentBooks*8*0.14);
+	}
+
+	if(differentBooks==4) {
+		sumToDiscount=(differentBooks*8*0.28);
+	}
+
+	if(differentBooks==5) {
+		sumToDiscount=(differentBooks*8*0.35);
+	}
+
+	sum=sum-sumToDiscount;
+	
+        return sum;
+    }
+}
